4a810d6613344843874c085567698ab608d74a79
Amal-Mousa <amalalsattari@gmail.com>
2023-04-07 12:39:03 +0300

    main file structure, create server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const {join} = require('path')
+const app = express();
+
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(join(__dirname, '../public'),{'max-age':'30d'}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.disable('x-powered-by');
+
+
+module.exports = { app };
+
+
+
